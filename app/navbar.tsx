5a3e73d512925c440f729c88d5236193066df5c3
ariefgp <arief.gusti@gmail.com>
2023-02-19 13:13:31 +0700

    add navbar

---
+++ app/navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+   <header className="hidden md:flex md:flex-row-reverse gap-16 my-8 mr-24"> 
+    <div>Artikel</div>
+    <div>Keluarga</div>
+    <div>Mbah Dasih</div>
+    <div>Mbah Daman</div>
+    <div>Beranda</div>
+   </header>
+  )
+}
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Invitation from './invitation/page'
+
+function Home() {
+  return (
+    <Invitation />
+  )
+}
+
+export default Home
