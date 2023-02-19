b2eb09679f796f2c169b27161e1de8641c7b9bcb
ariefgp <arief.gusti@gmail.com>
2023-02-19 13:13:38 +0700

    add invitation page

---
+++ app/invitation/page.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+function Invitation() {
+  return (
+    <div className="flex flex-col text-center">
+        <div className="basis-full">
+            <div className='flex flex-row'>
+                <div className="basis-1/2">
+                    <div className="">Sugeng Rawuh</div>
+                    <div>Warga <br /> IKADAMAR</div>
+                </div>
+                <div className="basis-1/2"></div>
+            </div>
+            
+        </div>
+        <div className="basis-full">
+            <div>
+                <div>Kami mengundang</div>
+                <div>Bapak, Ibu dan seluruh keluarga besar IKADAMAR ke acara</div>
+            </div>
+            <div>
+                <div>Reuni</div>
+                <div>Keluarga</div>
+            </div>
+            <div>
+                <div>yang akan dilaksanakan pada</div>
+            </div>
+            <div>
+                <div>
+                    <div>Minggu</div>
+                    <div>09:00</div>
+                </div>
+                <div>30</div>
+                <div>
+                    <div>April</div>
+                    <div>2023</div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    Kediaman Pak Musman dan Bu Dwi Asih
+                </div>
+                <div>
+                    Jl Soekarno Hatta Gang Duku No 15 Kelutan, Trenggalek
+                </div>
+                <div>
+                    Jawa Timur
+                </div>
+            </div>
+        </div>
+        <div className="basis-full">
+            <div>Hitung Mundur</div>
+            <div>Countdown</div>
+        </div>
+        <div className="basis-full">
+            <div>Di reuni keluarga kali ini, apakah berkenan hadir dengan keluarga?</div>
+            <div>
+                <button>Ya, bisa hadir</button>
+                <button>Belum bisa hadir</button>
+            </div>
+        </div>
+        <div className="basis-full">
+            Copyright @ariefgp. 2023
+        </div>
+    </div>
+  )
+}
+
+export default Invitation
