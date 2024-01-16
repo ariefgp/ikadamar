26cfeffac1f8a077e0e691761451d768d7766e52
ariefgp <arief.gusti@gmail.com>
2024-01-16 13:59:00 +0700

    update footer

--- app/invitation/page.tsx
+++ app/invitation/page.tsx
@@ -147,7 +147,7 @@ function Invitation() {
             </div>
         </div>
         <div className="basis-full px-5 py-3 md:py-5 bg-[#C7D1E1] text-white md:px-20 lg:px-48">
-            Copyright @ariefstudio. 2023
+            Copyright @codekarsa. 2024
         </div>
     </div>
   )
