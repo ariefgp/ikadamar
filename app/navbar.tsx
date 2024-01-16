61ca88baa458d9067ae099500c2d2fd653bd8869
ariefgp <arief.gusti@gmail.com>
2024-01-16 14:02:58 +0700

    disable link on Navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -5,21 +5,38 @@ function Navbar() {
   return (
    <header className="hidden md:flex md:flex-row-reverse gap-8 lg:gap-16 py-10 px-20 lg:py-16 lg:px-48 font-bold bg-[#DDE9FB] text-[#383333]"> 
     <div>
-        <Link href="/artikel" className='hover:text-[#7791BA]'> Artikel </Link>
+        <Link href="/#" className='hover:text-[#7791BA]'> Artikel </Link>
     </div>
     <div>
-        <Link href="/keluarga" className='hover:text-[#7791BA]'> Keluarga </Link>
+        <Link href="/#" className='hover:text-[#7791BA]'> Keluarga </Link>
     </div>
     <div>
-        <Link href="/eyang-dasih" className='hover:text-[#7791BA]'> Eyang Dasih </Link>
+        <Link href="/#" className='hover:text-[#7791BA]'> Eyang Dasih </Link>
     </div>
     <div>
-        <Link href="/eyang-daman" className='hover:text-[#7791BA]'> Eyang Daman </Link>
+        <Link href="/#" className='hover:text-[#7791BA]'> Eyang Daman </Link>
     </div>
     <div>
         <Link href="/" className='hover:text-[#7791BA]'> Beranda </Link>
     </div>
    </header>
+//    <header className="hidden md:flex md:flex-row-reverse gap-8 lg:gap-16 py-10 px-20 lg:py-16 lg:px-48 font-bold bg-[#DDE9FB] text-[#383333]"> 
+//         <div>
+//             <Link href="/artikel" className='hover:text-[#7791BA]'> Artikel </Link>
+//         </div>
+//         <div>
+//             <Link href="/keluarga" className='hover:text-[#7791BA]'> Keluarga </Link>
+//         </div>
+//         <div>
+//             <Link href="/eyang-dasih" className='hover:text-[#7791BA]'> Eyang Dasih </Link>
+//         </div>
+//         <div>
+//             <Link href="/eyang-daman" className='hover:text-[#7791BA]'> Eyang Daman </Link>
+//         </div>
+//         <div>
+//             <Link href="/" className='hover:text-[#7791BA]'> Beranda </Link>
+//         </div>
+//     </header>
   )
 }
 
