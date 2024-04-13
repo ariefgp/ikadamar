697f1713a230d4a7090543a346d715950378ece7
ariefgp <arief.gusti@gmail.com>
2024-04-13 15:38:30 +0700

    update the people style

--- app/people/page.tsx
+++ app/people/page.tsx
@@ -2,26 +2,29 @@ import Image from "next/image";
 
 const People = () => {
     return (
-        <div className="w-52 flex flex-col shadow-xl m-16">
-            <div>
-                <Image 
-                    src="https://img.freepik.com/free-photo/portrait-happy-asian-woman-smiling-posing-confident-cross-arms-chest-standing-against-studio-background_1258-88983.jpg" 
+        <div className="w-44 flex flex-col shadow-xl rounded-xl m-16 relative">
+            <div className="absolute top-[-30px] left-[-30px] z-10 w-44">
+                <Image
+                    src="https://img.freepik.com/free-photo/portrait-happy-asian-woman-smiling-posing-confident-cross-arms-chest-standing-against-studio-background_1258-88983.jpg"
                     alt="Main Image Ikadamar"
-                    height="800"
+                    height="400"
                     width="1200"
-                    className='md:-mb-14 lg:-mb-20'
+                    className='rounded-xl'
                 />
             </div>
-            <div>
-                <div> Dian Sastrowardoyo</div>
-                <div><hr /></div>
-                <div>
-                    <div></div>
-                    <div>Jl. Soekarno Hatta Gg Duku No 15 Kelutan Trenggalek</div>
-                </div>
-                <div>
-                    <div></div>
-                    <div>081234567889</div>
+            <div className="basis-full p-3 mt-20 text-left">
+                <div className="flex flex-col gap-2">
+                    <div className="basis-full font-semibold"> Dian Sastrowardoyo</div>
+                    <div><hr /></div>
+                    <div className="basis-full">
+                        <div></div>
+                        <div className="text-xs">Jl. Soekarno Hatta Gg Duku No 15 Kelutan Trenggalek</div>
+                    </div>
+                    <div className="basis-full">
+                        <div></div>
+                        <div className="text-xs">081234567889</div>
+                    </div>
+
                 </div>
             </div>
         </div>
