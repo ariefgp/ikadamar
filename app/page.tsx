8e76943d83f713fadd3371cc7e4924d1c0930102
ariefgp <arief.gusti@gmail.com>
2024-04-14 00:14:15 +0700

    get all data on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
-import Homepage from './main/Homepage'
+import { promises as fs } from 'fs';
+import Hero from './components/hero/Hero';
+import Footer from './components/footer/Footer';
+import People from './people/People';
 
-const Home = () => {
+const Home = async () => {
+  const file = await fs.readFile(process.cwd() + '/data/people.json', 'utf8');
+  const people = JSON.parse(file);
   return (
-    <Homepage />
+    <div className="flex flex-col text-center bg-[#f8f9fa] text-sm md:text-base">
+      <Hero />
+      <div className='basis-full px-5 md:px-20 mt-20'>
+        <div className="mb-12 font-semibold text-[#5F5B5B] text-2xl">Anggota Keluarga</div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 sm:gap-2 lg:gap-4">
+          {people?.slice(0, 4).map((person: any, index: any) => <People key={index} person={person} />)}
+        </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
